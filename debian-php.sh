5ba5afef179bfa2aea4aae86ce35e5e567c86031
ZotyaNET <49711103+ZotyaNET@users.noreply.github.com>
2024-11-17 09:30:06 +0100

    Create debian-php.sh

---
+++ debian-php.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# WEBMIN & VIRTUALMIN
+sudo sh -c "$(curl -fsSL https://software.virtualmin.com/gpl/scripts/virtualmin-install.sh) -- --bundle LAMP --hostname \"$1\""
+
+# PHP SOURCE
+apt-get -y install apt-transport-https lsb-release ca-certificates curl
+curl -sSL -o /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/sury-debian-php-$(lsb_release -sc).list'
+apt-get update
+
+# PHP VERSIONS
+sudo apt-get install -y php7.4-{cgi,cli,fpm,pdo,gd,mbstring,mysqlnd,opcache,sqlite3,bcmath,curl,xml,zip,intl}
+sudo apt-get install -y php8.0-{cgi,cli,fpm,pdo,gd,mbstring,mysqlnd,opcache,sqlite3,bcmath,curl,xml,zip,intl}
+sudo apt-get install -y php8.1-{cgi,cli,fpm,pdo,gd,mbstring,mysqlnd,opcache,sqlite3,bcmath,curl,xml,zip,intl}
+apt-get install -y php8.2-{cgi,cli,fpm,pdo,gd,mbstring,mysqlnd,opcache,sqlite3,bcmath,curl,xml,zip,intl}
+apt-get install -y php8.3-{cgi,cli,fpm,pdo,gd,mbstring,mysqlnd,opcache,sqlite3,bcmath,curl,xml,zip,intl}
+
+# INSTALLATRON
+wget https://data.installatron.com/installatron-plugin.sh
+chmod +x installatron-plugin.sh
+./installatron-plugin.sh -f
+/usr/local/installatron/installatron --convert
