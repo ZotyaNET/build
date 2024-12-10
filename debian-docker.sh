7b6774e53ffc3149072e323713ebe87dd84f5b2d
ZotyaNET <49711103+ZotyaNET@users.noreply.github.com>
2024-12-10 08:42:17 +0100

    Create debian-docker.sh

---
+++ debian-docker.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Wait ...
+sleep 2
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Wait ...
+sleep 2
+
+# latest
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
+
+# postinstall
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
