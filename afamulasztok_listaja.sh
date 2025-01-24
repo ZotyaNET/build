b42a3dd710b4d017b4973f5a21bd652275380c87
ZotyaNET <49711103+ZotyaNET@users.noreply.github.com>
2025-01-24 07:08:08 +0100

    Update afamulasztok_listaja.sh

--- afamulasztok_listaja.sh
+++ afamulasztok_listaja.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-tabula --outfile Afamulasztok_listaja.csv --lattice --use-line-returns --pages 1-10 Afamulasztok_listaja.pdf
-awk 'NR > 2 && $0 !~ /Adószám Adózó neve \(elnevezése\) Lakcím Székhely Telephely Illetékesség kódja/' Afamulasztok_listaja.csv > Afamulasztok_listaja.csv
-awk 'NR > 2 && $0 !~ /Adószám,Adózó neve \(elnevezése\),Lakcím,Székhely,Telephely,Illetékesség kódja/' Afamulasztok_listaja.csv > Afamulasztok_listaja.csv
+tabula --outfile Afamulasztok_listaja.csv --lattice --pages 1-10 Afamulasztok_listaja.pdf \
+&& awk 'NR > 2 && $0 !~ /Adószám Adózó neve \(elnevezése\) Lakcím Székhely Telephely Illetékesség kódja/' Afamulasztok_listaja.csv > Afamulasztok_listaja.csv \
+&& awk 'NR > 2 && $0 !~ /Adószám,Adózó neve \(elnevezése\),Lakcím,Székhely,Telephely,Illetékesség kódja/' Afamulasztok_listaja.csv > Afamulasztok_listaja.csv
