140a0db2c14948c49672aa62bf414ef746299ed1
ZotyaNET <49711103+ZotyaNET@users.noreply.github.com>
2025-01-24 08:00:57 +0100

    Update tabula.sh

--- tabula.sh
+++ tabula.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install g++ zip snap -y
+sudo apt-get install g++ zip snap default-mysql-client -y
 sudo snap install snapd
 sudo snap install tabula
