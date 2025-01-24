4dba27928ddcc9b20069f605a19ffce0083524f1
ZotyaNET <49711103+ZotyaNET@users.noreply.github.com>
2025-01-24 07:15:08 +0100

    Update afamulasztok_listaja.sh

--- afamulasztok_listaja.sh
+++ afamulasztok_listaja.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 # Extract data from PDF using Tabula
-tabula --outfile Afamulasztok_listaja.csv --lattice --pages 1-10 Afamulasztok_listaja.pdf
+tabula --outfile tmp1.csv --lattice --pages 1-10 Afamulasztok_listaja.pdf
 
 # Fix the line breaks
-tr -d '\r' < Afamulasztok_listaja.csv > Afamulasztok_listaja.csv
+tr -d '\r' < tmp1.csv > tmp2.csv
 
 # Remove the first two lines and lines containing specific headers
-awk 'NR > 2 && $0 !~ /Adószám Adózó neve \(elnevezése\) Lakcím Székhely Telephely Illetékesség kódja/' Afamulasztok_listaja.csv > Afamulasztok_listaja.csv
-awk 'NR > 2 && $0 !~ /Adószám,Adózó neve \(elnevezése\),Lakcím,Székhely,Telephely,Illetékesség kódja/' Afamulasztok_listaja.csv > Afamulasztok_listaja.csv
+awk 'NR > 2 && $0 !~ /Adószám Adózó neve \(elnevezése\) Lakcím Székhely Telephely Illetékesség kódja/' tmp2.csv > tmp3.csv
+awk 'NR > 2 && $0 !~ /Adószám,Adózó neve \(elnevezése\),Lakcím,Székhely,Telephely,Illetékesség kódja/' tmp3.csv > tmp4.csv
+
+# Remove tmp files
+mv tmp4.csv afamulasztok_listaja.csv
+rm tmp3.csv
+rm tmp2.csv
+rm tmp1.csv
