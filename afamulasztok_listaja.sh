0fd8bdda3bcf712df8fa1daa17ac16689b427009
ZotyaNET <49711103+ZotyaNET@users.noreply.github.com>
2025-01-24 07:42:40 +0100

    Update afamulasztok_listaja.sh

--- afamulasztok_listaja.sh
+++ afamulasztok_listaja.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Extract data from PDF using Tabula
-tabula --outfile Afamulasztok_listaja.csv --lattice --pages all Afamulasztok_listaja.pdf
+tabula --outfile temp.csv --lattice --pages all Afamulasztok_listaja.pdf
+
+# Fix line endings
+tr -d '\r' < temp.csv > Afamulasztok_listaja.csv
 
 # Remove the first two lines and lines containing specific headers
 awk 'NR > 2 && $0 !~ /Adószám Adózó neve \(elnevezése\) Lakcím Székhely Telephely Illetékesség kódja/' Afamulasztok_listaja.csv > temp.csv
