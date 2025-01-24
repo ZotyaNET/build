071ddf45928541d95dfb0a7dc8115e7e1c00e212
ZotyaNET <49711103+ZotyaNET@users.noreply.github.com>
2025-01-24 07:13:13 +0100

    Update afamulasztok_listaja.sh

--- afamulasztok_listaja.sh
+++ afamulasztok_listaja.sh
@@ -3,9 +3,9 @@
 # Extract data from PDF using Tabula
 tabula --outfile Afamulasztok_listaja.csv --lattice --pages 1-10 Afamulasztok_listaja.pdf
 
-# Remove the first two lines and lines containing specific headers
-awk 'NR > 2 && $0 !~ /Adószám Adózó neve \(elnevezése\) Lakcím Székhely Telephely Illetékesség kódja/' Afamulasztok_listaja.csv > temp.csv
-awk 'NR > 2 && $0 !~ /Adószám,Adózó neve \(elnevezése\),Lakcím,Székhely,Telephely,Illetékesség kódja/' temp.csv > Afamulasztok_listaja.csv
+# Fix the line breaks
+tr -d '\r' < Afamulasztok_listaja.csv > Afamulasztok_listaja.csv
 
-# Remove the temporary file
-rm temp.csv
+# Remove the first two lines and lines containing specific headers
+awk 'NR > 2 && $0 !~ /Adószám Adózó neve \(elnevezése\) Lakcím Székhely Telephely Illetékesség kódja/' Afamulasztok_listaja.csv > Afamulasztok_listaja.csv
+awk 'NR > 2 && $0 !~ /Adószám,Adózó neve \(elnevezése\),Lakcím,Székhely,Telephely,Illetékesség kódja/' Afamulasztok_listaja.csv > Afamulasztok_listaja.csv
